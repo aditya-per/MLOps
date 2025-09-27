eccd91374a4b054abf093ac7ce830586fa412682
aditya-per <aditya_511@hotmail.com>
2025-09-27 09:18:41 +0530

    Third commit

--- tourism_project/model_building/train.py
+++ tourism_project/model_building/train.py
@@ -19,8 +19,8 @@ from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
 import mlflow
 
 #Using ngroc link from previous step to send stats to MLflow running on local machine
-mlflow.set_tracking_uri("https://8918b9a2000a.ngrok-free.app")
-mlflow.set_experiment("mlops-training-experiment2")
+mlflow.set_tracking_uri("https://a833b888a544.ngrok-free.app")
+mlflow.set_experiment("mlops-training-experiment3")
 
 api = HfApi()
 
@@ -104,12 +104,12 @@ with mlflow.start_run():
         std_score = results['std_test_score'][i]
 
         # Log each combination as a separate MLflow run
-        #with mlflow.start_run(nested=True):
-            #mlflow.log_params(param_set)
-            #mlflow.log_metric("mean_test_score", mean_score)
-            #mlflow.log_metric("std_test_score", std_score)
+        with mlflow.start_run(nested=True):
+            mlflow.log_params(param_set)
+            mlflow.log_metric("mean_test_score", mean_score)
+            mlflow.log_metric("std_test_score", std_score)
             # Adding 3 second sleep as ngroc has a rate limit of 120 calls per miniute
-            #time.sleep(5) 
+            time.sleep(2) 
 
     # Log best parameters separately in main run
     mlflow.log_params(grid_search.best_params_)
